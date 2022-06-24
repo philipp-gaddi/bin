89dfe972e5cff51e650a1d485378420001132547
Philipp Reckziegel <philipp.reckziegel@mail.de>
2022-06-24 14:06:32 +0200

    initial commit

---
+++ key_gen2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+
+notes_list = ['C', 'C#', 'D', 'Db', 'D#', 'E', 'Eb', 'F', 'F#', 'G', 'Gb', 'G#', 'A', 'Ab', 'A#', 'B', 'Bb']
+
+
+
---
+++ tone_gen.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+tone_set = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A#', 'Ab', 'Bb', 'C#', 'Db', 'D#', 'Eb', 'F#', 'Gb', 'G#']
+
+print(np.random.choice(tone_set))
+
+
+
