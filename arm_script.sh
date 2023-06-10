21c681e4cbf2a141566821504c0ddabf11920d10
Philipp Reckziegel <philipp.reckziegel@mail.de>
2023-06-10 17:02:52 +0200

    update

--- arm_script.sh
+++ arm_script.sh
@@ -6,6 +6,9 @@ combine_images() {
   local path_from_root="$(pwd)"
 
   # finding the ambient occlusion, roughness and metal textures, case insensitive, and variations of the suffix
+  # suffix can be _ao, -ao, Ambient for Ambient Occlusion Maps
+  # suffix can be _rough, -rough, Rough for Rougness Maps
+  # suffix can be _metal, -metal, Metal for Metal Maps
   local ao_file=$(find "$folder" -type f \( -iname "*_ao*" -o -iname "*-ao*" \) -print -quit)
   if [[ -n "$ao_file" ]]; then
 	ao_file="${path_from_root}/${ao_file}"
@@ -27,6 +30,7 @@ combine_images() {
   fi
 
   # constructing the outputfilepath 
+  # if at least one of the textures were found, the arm texture can be created.
   local base_name=""
   local file_extension=""
   
@@ -50,29 +54,25 @@ combine_images() {
   local output_file="${base_name}${output_suffix}.${file_extension}"
 
   # creating arguments for convert
-  local args=($ao_file $rough_file $metal_file -combine $output_file ) 
+  local args=(${ao_file} ${rough_file} ${metal_file} -combine ${output_file} ) 
 
   # creating the arm_file with convert and the args
   convert "${args[@]}" 2>/dev/null
 
-  if [ $? -eq 0 ]; then
-    echo "Combined image created: $output_file"
-  else
-	echo "no combined image created for: $output_file"
+  if [ ! $? -eq 0 ]; then
+	echo "no combined image created for: ${output_file}"
+	return 1
   fi
+
+  return 0
 }
 
 traverse_folder() {
   local folder="$1"
 
-  # Check if a folder is provided as an argument
-  if [ $# -eq 0 ]; then
-    echo "Please provide a folder as an argument."
-    return 1
-  fi
-
-  # looping through all subfolders and applying combine images in the leaf folders
-  find "$folder" -mindepth 1 -type d -not -path '*/.*' | while read -r dir; do
+  # looping through all folders and applying combine images in the leaf folders
+  # ignoring hidden folders
+  find "$folder" -type d -not -path '*/.*' | while read -r dir; do
     if ! find "$dir" -mindepth 1 -type d | read; then
       combine_images "$dir"
     fi
